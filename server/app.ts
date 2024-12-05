9ef16270f1d9fab9bfe02ae1c9b656f39eb1b9e0
Anton Bezborody <anton.bezborody@gmail.com>
2024-12-06 00:36:47 +0300

    auth init, link to kinde

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono"
 import { logger } from "hono/logger"
 import { serveStatic } from "hono/bun"
 import { exprensesRoute } from "./routes/expenses"
+import { authRoute } from "./routes/auth"
 
 // Init app
 const app = new Hono()
@@ -9,7 +10,10 @@ const app = new Hono()
 app.use("*", logger())
 
 // Routes
-const apiRoutes = app.basePath("/api").route("/expenses", exprensesRoute)
+const apiRoutes = app
+  .basePath("/api")
+  .route("/expenses", exprensesRoute)
+  .route("/", authRoute)
 
 app.get("*", serveStatic({ root: "./frontend/dist" }))
 app.get("*", serveStatic({ path: "./frontend/dist/index.html" }))
